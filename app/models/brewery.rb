dbc220b2718efb9bce50a4668018177d8e438a47
minkamanki <minka.tahvanainen@gmai.com>
2022-10-19 19:08:09 +0300

    Added validation

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,7 +2,17 @@ class Brewery < ApplicationRecord
   include RatingAverage
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+  validates :name, presence: true, allow_blank: false
+  validates :year, numericality: { greater_than_or_equal_to: 1040,
+                                   only_integer: true }
+  validate :year_cannot_be_in_the_future, on: :create
 
+  def year_cannot_be_in_the_future
+    if year.present? && year > Date.today.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
+                              
   def print_report
     puts name
     puts "established at year #{year}"
