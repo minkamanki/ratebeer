4f8082c60ac8e74e4213a6e8b01cb54713a45aaa
minkamanki <minka.tahvanainen@gmai.com>
2022-10-19 15:20:01 +0300

    Added Sign out

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   get 'signup', to: 'users#new'
   resource :session, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
